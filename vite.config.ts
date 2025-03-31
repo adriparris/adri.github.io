81a2eae080e831d74550a9658b810c7a99df0204
Adri Parris <adriparris@Adris-MacBook-Pro.local>
2025-03-31 20:49:06 +0200

    Deploying portfolio

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import compression from "vite-plugin-compression";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    compression(), // Enables Brotli & Gzip compression
   ],
-  
-})
+  build: {
+    chunkSizeWarningLimit: 500, // Prevents large chunk warnings
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor"; // Moves dependencies into a separate file
+          }
+        },
+      },
+    },
+  },
+});
